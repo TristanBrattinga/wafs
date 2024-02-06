f27e5daa878c31b9cf8faf07228b43e7b50df0e5
TristanBrattinga <tristan.brattinga@gmail.com>
2024-02-06 15:08:04 +0100

    added error handling and env

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,22 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app  = express()
-const port = 3000
+app.use(express.static('public'))
 
-app.set('view engine', 'ejs')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my server!')
-})
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.use((req, res) => {
+  res.status(404).send('We`re sorry, we were not able to find the page you were looking for');
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is listening to port: ${port}`);
+});
