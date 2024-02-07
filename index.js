b94b2353e350dadb8f65b6566442a51ae6d86d45
TristanBrattinga <tristan.brattinga@gmail.com>
2024-02-07 10:38:21 +0100

    vercel deploy ready wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const api  = require('./api')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -17,6 +18,10 @@ app.use((req, res) => {
   res.status(404).send('We`re sorry, we were not able to find the page you were looking for');
 });
 
+app.use('/api', api)
+
 app.listen(port, () => {
   console.log(`Server is listening to port: ${port}`);
 });
+
+module.exports = app
