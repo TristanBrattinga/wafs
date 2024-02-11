e8bd65109e8e69bd8cc5b9d9a6b93eb5d6b19e56
TristanBrattinga <tristan.brattinga@gmail.com>
2024-02-11 01:28:11 +0100

    fetch movie data and display

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,28 +4,6 @@ const hamburgerMenu       = document.querySelector('.hamburgerMenu')
 const darkModeButton      = document.querySelector('.darkModeToggle')
 const scrollToTopButton   = document.querySelector('.scrollToTop')
 
-const favouriteMovies = ['return-of-the-jedi', 'i-am-legend', 'avengers-endgame', 'the-hateful-eight']
-
-const fetchPublicApi = () => {
-  favouriteMovies.map((movie) => {
-    fetch(`http://www.omdbapi.com/?apikey=12dd510b&t=${movie}`)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error
-          (`HTTP error! Status: ${res.status}`)
-        }
-        return res.json()
-      })
-      .then((data) => {
-        // console.log(data)
-      })
-      .catch((e) => console.error('Unable to fetch data:', e))
-
-  })
-}
-
-fetchPublicApi()
-
 const toggleHamburgerMenu = () => {
   hamburgerMenu.classList.toggle('slideOut')
   hamburgerMenuButton.classList.toggle('menuOpen')
--- server.js
+++ server.js
@@ -8,13 +8,36 @@ app.use(express.static('public'))
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
+const favouriteMovies = ['return-of-the-jedi', 'i-am-legend', 'avengers-endgame', 'the-hateful-eight']
+let movies            = []
+
+const fetchMovies = () => {
+    favouriteMovies.map((movie) => {
+      fetch(`http://www.omdbapi.com/?apikey=12dd510b&t=${movie}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error
+            (`HTTP error! Status: ${res.status}`)
+          }
+          return res.json()
+        })
+        .then((movieData) => {
+          movies.push(movieData)
+          console.log(movies)
+          return movies
+        })
+        .catch((e) => console.error('Unable to fetch data:', e))
+    })
+}
+
+fetchMovies()
+
 // index page
 app.get('/', async (req, res) => {
   try {
     const response = await fetch('https://tristanbrattinga.github.io/wafs/data/data.json')
-    const data = await response.json()
-    console.log(data)
-    res.render('pages/index', { data: data })
+    const data     = await response.json()
+    res.render('pages/index', { data: data, movies: movies })
   } catch (e) {
     console.error('Unable to fetch the data:', e)
   }
