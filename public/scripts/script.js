7cf560e3cbe8680bef69c68ba5cb588caab4b3ed
TristanBrattinga <tristan.brattinga@gmail.com>
2024-02-12 23:17:37 +0100

    carousel swyling wip

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,6 +3,13 @@ const hamburgerMenuButton = document.querySelector('.hamburgerMenuButton')
 const hamburgerMenu       = document.querySelector('.hamburgerMenu')
 const darkModeButton      = document.querySelector('.darkModeToggle')
 const scrollToTopButton   = document.querySelector('.scrollToTop')
+const slider = document.querySelector('.movieImages')
+const slides = document.querySelector('.movies article section ul li')
+const prevButton = document.getElementById("prevButton")
+const nextButton = document.getElementById("nextButton")
+const dots = document.querySelector('.movies span')
+
+dots.style.backgroundColor = 'red'
 
 const toggleHamburgerMenu = () => {
   hamburgerMenu.classList.toggle('slideOut')
@@ -32,3 +39,18 @@ const handleScrollToTop = () => {
 }
 
 scrollToTopButton.addEventListener('click', handleScrollToTop)
+
+/////////////// MOVIE SLIDER ///////////////
+
+let currentSlide = 0
+
+const handleSlidePrev = () => {
+
+}
+
+const handleSlideNext = () => {
+
+}
+
+nextButton.addEventListener('click', handleSlidePrev)
+nextButton.addEventListener('click', handleSlideNext)
--- server.js
+++ server.js
@@ -1,53 +1,74 @@
+// Basic express server setup
 require('dotenv').config()
 const express = require('express')
 const app     = express()
 const port    = process.env.PORT || 3000
 
+// Function to assign static folder
 app.use(express.static('public'))
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
+// Define favourite movies
 const favouriteMovies = ['return-of-the-jedi', 'i-am-legend', 'avengers-endgame', 'the-hateful-eight']
-let movies            = []
 
+// Empty array that will house the fetched movie data
+let movies = []
+
+// Empty object that will house the fetched personal data
+let personalData = {}
+
+// Function that maps over the favourite movies array and pushes them into the movies array
 const fetchMovies = () => {
-    favouriteMovies.map((movie) => {
-      fetch(`http://www.omdbapi.com/?apikey=12dd510b&t=${movie}`)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error
-            (`HTTP error! Status: ${res.status}`)
-          }
-          return res.json()
-        })
-        .then((movieData) => {
-          movies.push(movieData)
-          console.log(movies)
-          return movies
-        })
-        .catch((e) => console.error('Unable to fetch data:', e))
-    })
+  favouriteMovies.map((movie) => {
+    fetch(`http://www.omdbapi.com/?apikey=12dd510b&t=${movie}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error
+          (`HTTP error! Status: ${res.status}`)
+        }
+        return res.json()
+      })
+      .then((movieData) => {
+        movies.push(movieData)
+        return movies
+      })
+      .catch((e) => console.error('Unable to fetch data:', e))
+  })
 }
 
-fetchMovies()
+// Function that fetches personal JSON data
+const fetchPersonalData = () => {
+  fetch('https://tristanbrattinga.github.io/wafs/data/data.json').then((res) => {
+      if (!res.ok) {
+        throw new Error
+        (`HTTP error! Status: ${res.status}`)
+      }
+      return res.json()
+    })
+    .then((data) => {
+      personalData = data
+      return personalData
+    })
+    .catch((e) => console.error('Unable to fetch data:', e))
+}
 
-// index page
+// Function calls
+fetchMovies()
+fetchPersonalData()
+
+// Route handle for the index page
 app.get('/', async (req, res) => {
-  try {
-    const response = await fetch('https://tristanbrattinga.github.io/wafs/data/data.json')
-    const data     = await response.json()
-    res.render('pages/index', { data: data, movies: movies })
-  } catch (e) {
-    console.error('Unable to fetch the data:', e)
-  }
+  res.render('pages/index', { data: personalData, movies: movies })
 })
 
-// Error handling for not-existing pages
+// Error route handling for not-existing pages
 app.use((req, res) => {
   res.status(404).send('We`re sorry, we were not able to find the page you were looking for')
 })
 
+// Function to start up the server, HELLO WORLD!
 app.listen(port, () => {
   console.log(`Server is listening to port: ${port}`)
 })
